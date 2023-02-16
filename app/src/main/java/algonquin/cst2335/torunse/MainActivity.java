7fd5021c03bffbacbb4202ac46e64944b3ac1f81
etorunski <etorunski@gmail.com>
2023-02-15 19:37:38 -0500

    finished recycler view

--- app/src/main/java/algonquin/cst2335/torunse/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/torunse/MainActivity.java
@@ -1,11 +1,27 @@
 package algonquin.cst2335.torunse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import algonquin.cst2335.torunse.data.ChatViewModel;
+import algonquin.cst2335.torunse.databinding.ActivityMainBinding;
+import algonquin.cst2335.torunse.databinding.RowLayout1Binding;
+import algonquin.cst2335.torunse.databinding.RowLayoutBinding;
 
 /** This is a page that simulates a login page
  * @author Eric Torunski
@@ -14,55 +30,95 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
-
     /** This holds the edit text where the user puts their password **/
-    private EditText thePasswordText;
-
+    private ArrayList<String> messageList ;
+    private RecyclerView.Adapter myAdapter ;
     @Override  //This starts the application
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActivityMainBinding binding = ActivityMainBinding.inflate(
+                getLayoutInflater());
+
+        ChatViewModel cvm  = new ViewModelProvider(this).get(ChatViewModel.class);
+        messageList = cvm.messages;
+
         /*loads buttons / text on screen */
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
                     //R means res
                     //layout is the folder
                     //activity_main is the file
 
-       thePasswordText = findViewById(R.id.editText);
-       Button btn = findViewById(R.id.button);
+        binding.theRecycleView.setAdapter( myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override   //which XML layout for this row? row_layout.xml, comes from below
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                if(viewType == 0) {
+                    //always inflates row_layout.xml
+                    RowLayoutBinding binding = RowLayoutBinding.inflate(getLayoutInflater(),
+                            parent, false
 
-       btn.setOnClickListener( clk -> {
-            String password = thePasswordText.getText().toString();
+                    );
+                    View root = binding.getRoot();
+                    return new MyRowHolder(root);
+                }
+                else{
+                    RowLayout1Binding binding = RowLayout1Binding.inflate(getLayoutInflater(),
+                            parent, false
 
-            //looking for Uppercase, lowercase, numbber, and special character
+                    );
+                    View root = binding.getRoot();
+                    return new MyRowHolder(root);
+                }
+            }
 
-           if(verifyPassword(password))
-           {
-               //password has an uppercase character
-               Toast.makeText(this, "I found an upper case!", Toast.LENGTH_SHORT).show();
-           }
+            @Override  //what are the textViews set to for row POSITION?
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+                    String messageOnThisRow = messageList.get(position);//which string goes in this row?
 
-       });
+
+                holder.messageText.setText(messageOnThisRow);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+                String currentDateandTime = sdf.format(new Date());
+                holder.timeText.setText(currentDateandTime);
+            }
+
+            @Override
+            public int getItemCount() { //how big is this list
+                return messageList.size();
+            }
+
+            @Override   //which layout to load at row position
+            public int getItemViewType(int position) {
+                //you can return anything to represent a layout
+                return position % 2;
+            }
+        });
+
+        binding.theRecycleView.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.sendButton.setOnClickListener( clk ->{
+            String txt =  binding.editText.getText().toString();
+            messageList.add(txt);
+
+            //redraw the whole list
+            myAdapter.notifyItemInserted( messageList.size() - 1 );
+
+            binding.editText.setText(""); //now will be empty
+
+        });
     }
 
-    /** This function scans the string to see if it is complex enough
-     *
-     * @param pw The string to verify
-     * @return True if pw has uppercase, otherwise false
-     */
-   public boolean verifyPassword(String pw){
-        boolean isComplexEnough = false;
+    class MyRowHolder extends RecyclerView.ViewHolder{
 
-        for(int i = 0; i < pw.length(); i++)
-        {
-            char c = pw.charAt(i);
-
-            if(Character.isUpperCase(c))
-                isComplexEnough = true;
+        public TextView messageText;
+        public TextView timeText;
+        public MyRowHolder(@NonNull View itemView) { //itemView will the the root of the layout, ConstraintLayout
+            super(itemView);
+            messageText = itemView.findViewById(R.id.message);
+            timeText = itemView.findViewById(R.id.time);
         }
-        return isComplexEnough;
     }
-
-
 
 }
---
+++ app/src/main/java/algonquin/cst2335/torunse/data/ChatViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.torunse.data;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatViewModel extends ViewModel {
+    public ArrayList<String> messages = new ArrayList<>();
+}
